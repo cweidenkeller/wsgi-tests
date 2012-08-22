cf46ab858cef2dca08c1ae50e377dfa49b5f1482
Conrad Weidenkeller <conrad.weidenkeller@rackspace.com>
2012-08-21 22:18:43 -0500

    updated some stuff so this works again

--- configured.py
+++ configured.py
@@ -3,28 +3,18 @@ from pprint import pprint
 from flask import Flask
 import sys
 app = Flask(__name__)
-
+app.debug = True
 @app.route('/')
 def hello_world():
     raise Exception
     return 'woo'
-
-class Configured:
-    def __init__(self, name, greeting):
-        self.name = name
-        self.greeting = greeting
-    def __call__(self, environ, start_response):
-        status = '200 OK'
-        response_headers = [('Content-Type', 'text/plain')]
-        start_response(status, response_headers)
-        return ['%s, %s!\n' % (self.greeting, self.name)]
 class Caseless:
     def __init__(self, app):
         self.app = app
-    def __call__(self, environ, start_response):
+    def __call__(self, environ, start_response, exc_info=None):
         pprint(environ)
         rr = 'apples'
-        if sys.exc_info()[0] != None:
+        if exc_info[0] != None:
             rr = 'jjjjjj'
             raise ZeroDivisionError
         status = '200 OK'
@@ -35,7 +25,7 @@ class Caseless:
             a += i
         #start_response(status, response_headers)
         return[rr + a ]
-def app_factory(global_config):
+def app_factory(global_config, name, greeting):
     return app
 def filter_factory(app, global_config):
     return Caseless(app)  
